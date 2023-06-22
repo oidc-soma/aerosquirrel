112797f2146083b2b8d56d8b81476902fe1e7024
hankn01 <kangminpersonal@naver.com>
2023-06-22 16:45:55 +0900

    Main Screen Navbar added

---
+++ client/aerosquirrel_client/src/components/navbar/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link, Route } from 'react-router-dom';
+
+function Navbar() {
+    // Global App Context 정의 예정
+        
+return (
+    <nav>
+        <div className="flex items-center gap-8 text-sm font-semibold text-black-400" rel="noopener noreferrer">
+           <div>
+            링크 로고 이미지 삽입예정
+           </div>
+        </div>
+    </nav>
+)
+}
+
+export default Navbar;
