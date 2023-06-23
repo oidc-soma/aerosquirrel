faebe5f39cb47164900328d51af5275609dd9ed7
hankn01 <kangminpersonal@naver.com>
2023-06-23 16:06:21 +0900

    React Router Set

--- client/aerosquirrel_client/src/App.js
+++ client/aerosquirrel_client/src/App.js
@@ -3,12 +3,18 @@ import React from 'react';
 import Navbar from './components/navbar/Navbar';
 import './App.css';
 import Accounts from './pages/Accounts';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Navbar />
-      <Accounts />
+      <Routes>
+        <Route path="/" element={<Accounts />}></Route>
+        <Route path="/accounts/*" element={<Accounts />}></Route>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
