f8b574e8fa68f2143ab99e592e0f9ae152008a96
hankn01 <kangminpersonal@naver.com>
2023-06-25 14:18:43 +0900

    Dashboard layout implemented

--- client/aerosquirrel_client/src/pages/Dashboard.tsx
+++ client/aerosquirrel_client/src/pages/Dashboard.tsx
@@ -1,12 +1,73 @@
 import React from 'react';
+import Cards from '../components/cards/Cards';
+import './Dashboard.css';
+import styled from 'styled-components';
+
+const DashboardLabel = styled.h1`
+  position: absolute;
+  left: 8rem;
+  top: 1rem;
+
+  font-size: 20px;
+`;
+
 
 function Dashboard() {
     
-
-    return(
-        <>
-        </>
-    )
+    return (
+      <>
+      <DashboardLabel>Dashboard</DashboardLabel>
+        <div
+          className="dashboardGrid"
+          style={{
+            marginLeft: "140px",
+            paddingTop: "110px",
+            paddingRight: "5rem",
+          }}
+        >
+          {[...Array(4)].map((_, i) => (
+            <Cards
+              key={i}
+              HeaderTitle="Cloud"
+              SecondaryTitle="Information"
+              Content="Dashboard"
+            />
+          ))}
+        </div>
+        <div
+          className="mapchartGrid"
+          style={{
+            marginLeft: "140px",
+            paddingTop: "30px",
+            paddingRight: "5rem",
+          }}
+        >
+          {[...Array(2)].map((_, i) => (
+            <Cards
+              key={i}
+              HeaderTitle="Cloud"
+              SecondaryTitle="Cost Info"
+              Content="Dashboard"
+            />
+          ))}
+        </div>
+        <div
+          className="costexplorerGrid"
+          style={{
+            marginLeft: "140px",
+            paddingTop: "30px",
+            paddingRight: "5rem",
+          }}
+        >
+          <Cards
+            key={0}
+            HeaderTitle="Cloud"
+            SecondaryTitle="Cost Info"
+            Content="Dashboard"
+          />
+        </div>
+      </>
+    );
 }
 
 export default Dashboard;
--- client/aerosquirrel_client/src/pages/Settings.tsx
+++ client/aerosquirrel_client/src/pages/Settings.tsx
@@ -1,8 +1,59 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const SettingsLabel = styled.h1`
+  position: absolute;
+  left: 8rem;
+  top: 1rem;
+
+  font-size: 20px;
+`;
+
+const PrefLabel = styled.h1`
+  position: absolute;
+  left: 8rem;
+  top: 5rem;
+  font-size: 15px;
+`
+
+const UserNameLabel = styled.h1`
+  position: absolute;
+  left: 8rem;
+  top: 10rem;
+  font-size: 15px;
+`;
+
+const UserNameContentLabel = styled.h1`
+  position: absolute;
+  left: 8rem;
+  top: 12rem;
+  font-size: 25px;
+`;
+
+const Hr = styled.hr`
+    position: absolute;
+    left: 0px;
+    top: 20rem;
+    width: 2px;
+`;
+
+const UserBillLine = styled.div`
+    position: absolute;
+    left: 300px;
+    top: 20rem;
+    background-color: black;
+`
 
 function Settings() {
     return(
         <>
+        <SettingsLabel>Settings</SettingsLabel>
+        <PrefLabel>Profile</PrefLabel>
+        <UserNameLabel>Username </UserNameLabel>
+        <UserNameContentLabel>TestUSER</UserNameContentLabel>
+        <UserBillLine>___</UserBillLine>
+        
+        <hr />
         </>
     )
 }
