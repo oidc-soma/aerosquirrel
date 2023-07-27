bc4d9151ff4f8c4e23514dae9eef5234a83c4bc7
hankn01 <kangminpersonal@naver.com>
2023-07-27 23:42:44 +0900

    Login form axios POST implemented

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Form from "react-bootstrap/Form";
 import { Button } from "react-bootstrap";
 import './Login.css';
+import axios from "axios";
 
 const LoginLabel = styled.h1`
   position: absolute;
@@ -11,6 +12,24 @@ const LoginLabel = styled.h1`
 `;
 
 function Login() {
+
+    const [LoginEmail, setLoginEmail] = useState('');
+    const [LoginPassword, setLoginPassword] = useState('');
+
+        const EmailInputHandler = (e:any) => {
+          setLoginEmail(e.currentTarget.value);
+        };
+        const PasswordInputHandler = (e:any) => {
+          setLoginPassword(e.currentTarget.value);
+        };
+
+
+    const LoginPostExecute = () => {
+      axios.post("https://d9c25fa3-a939-4ec2-abd9-a479b24bdf39.mock.pstmn.io/api/v1/login", {username: LoginEmail, password: LoginPassword})
+      .then(function(response) {
+        console.log(response);
+      });
+    };
 
     return (
       <>
@@ -23,15 +42,16 @@ function Login() {
           <br />
           <Form>
             <Form.Group controlId="signupForm.ControlInput">
-              <Form.Control type="text" placeholder="Username"></Form.Control>
+              <Form.Control type="text" placeholder="Username" onChange={EmailInputHandler}></Form.Control>
               <br />
               <Form.Control
                 type="password"
                 placeholder="Password"
+                onChange={PasswordInputHandler}
               ></Form.Control>
               <br />
               <br />
-              <Button>Login</Button>
+              <Button onClick={LoginPostExecute}>Login</Button>
               <Button variant="light">Cancel</Button>
             </Form.Group>
           </Form>
