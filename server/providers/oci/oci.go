1638198a77f1219f194e6249e196184910e405ed
mye280c37 <mye785c83@gmail.com>
2023-08-02 13:18:54 +0900

    Add function which fetch oci supported services (#20)

--- server/providers/oci/oci.go
+++ server/providers/oci/oci.go
@@ -1,13 +1,30 @@
 package oci
 
 import (
+	"context"
 	"github.com/oidc-soma/aerosquirrel/server/models"
+	"github.com/oidc-soma/aerosquirrel/server/providers/oci/compute"
+	"github.com/oidc-soma/aerosquirrel/server/providers/oci/developerservices"
+	"github.com/oidc-soma/aerosquirrel/server/providers/oci/iam"
+	"github.com/oidc-soma/aerosquirrel/server/providers/oci/oracledatabase"
+	"github.com/oidc-soma/aerosquirrel/server/providers/oci/storage"
 	"github.com/oracle/oci-go-sdk/common"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type Provider struct {
 	Client common.ConfigurationProvider
+}
+
+type FetchDataFunction func(ctx context.Context, p Provider, teamId primitive.ObjectID) ([]*models.Resource, error)
+
+var supportedServices = []FetchDataFunction{
+	compute.FetchInstances,
+	iam.FetchPolicies,
+	oracledatabase.FetchAutonomousDatabases,
+	storage.FetchBuckets,
+	storage.FetchBlockVolumes,
+	developerservices.FetchApplications,
 }
 
 func NewProvider() *Provider {
@@ -18,6 +35,15 @@ func NewProvider() *Provider {
 
 func (p *Provider) FetchResources(teamId primitive.ObjectID) ([]*models.Resource, error) {
 	resources := make([]*models.Resource, 0)
+	ctx := context.Background()
+
+	for _, service := range supportedServices {
+		ociResources, err := service(ctx, *p, teamId)
+		if err != nil {
+			return resources, err
+		}
+		resources = append(resources, ociResources...)
+	}
 
 	return resources, nil
 }
