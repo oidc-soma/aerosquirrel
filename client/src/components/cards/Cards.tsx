553be57a1644c9ff90c582f0bfd20d5fb5f844cd
hankn01 <kangminpersonal@naver.com>
2023-08-06 16:27:27 +0900

    Account Item box animation bug fixed

--- client/src/components/cards/Cards.tsx
+++ client/src/components/cards/Cards.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import { ReactComponent as AWS } from '../../AWS.svg';
 import { ReactComponent as Oracle} from '../../Oracle.svg';
+import './Cards.css';
 
 type CardsProps = {
     HeaderTitle: string,
@@ -15,41 +16,47 @@ function Cards({HeaderTitle, SecondaryTitle, Content}:CardsProps) {
 if(Content==="AWS")
 {
     return (
-      <Card border="info">
-        <Card.Header>{HeaderTitle}</Card.Header>
-        <Card.Body>
-          <Card.Title>
-            <AWS></AWS>
-          </Card.Title>
-          <Card.Text>{SecondaryTitle}</Card.Text>
-        </Card.Body>
-      </Card>
+      <div className="CLDWrapper">
+        <Card border="info">
+          <Card.Header>{HeaderTitle}</Card.Header>
+          <Card.Body>
+            <Card.Title>
+              <AWS></AWS>
+            </Card.Title>
+            <Card.Text>{SecondaryTitle}</Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
     );
 
 }else if(Content==="Oracle"){
   return (
-    <Card border="info">
-      <Card.Header>{HeaderTitle}</Card.Header>
-      <Card.Body>
-        <Card.Title>
-          <Oracle></Oracle>
-        </Card.Title>
-        <Card.Text>{SecondaryTitle}</Card.Text>
-      </Card.Body>
-    </Card>
+    <div className="CLDWrapper">
+      <Card border="info">
+        <Card.Header>{HeaderTitle}</Card.Header>
+        <Card.Body>
+          <Card.Title>
+            <Oracle></Oracle>
+          </Card.Title>
+          <Card.Text>{SecondaryTitle}</Card.Text>
+       </Card.Body>
+      </Card>
+      </div>
   );
 
 
 }
 else{
 return (
-  <Card border="info">
-    <Card.Header>{HeaderTitle}</Card.Header>
-    <Card.Body>
-      <Card.Title>{SecondaryTitle}</Card.Title>
-      <Card.Text>{"Other"}</Card.Text>
-    </Card.Body>
-  </Card>
+  <div className="CLDWrapper">
+    <Card border="info">
+      <Card.Header>{HeaderTitle}</Card.Header>
+      <Card.Body>
+        <Card.Title>{SecondaryTitle}</Card.Title>
+        <Card.Text>{"Other"}</Card.Text>
+      </Card.Body>
+    </Card>
+  </div>    
 );
 
 }
